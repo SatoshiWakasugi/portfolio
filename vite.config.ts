3e86965455ada8b8db7b6022882aa479a12debd8
SatoshiWakasugi <waka0628s@gmail.com>
2024-11-22 10:17:10 +0900

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { copyFileSync } from "node:fs";
-import { join } from "node:path";
 
 declare module "@remix-run/node" {
   interface Future {
@@ -11,18 +9,8 @@ declare module "@remix-run/node" {
 }
 
 export default defineConfig({
-  base: "/portfolio/",
   plugins: [
     remix({
-      basename: "/portfolio/",
-      buildEnd(args) {
-        if (!args.viteConfig.isProduction) return;
-        const buildPath = args.viteConfig.build.outDir;
-        copyFileSync(
-          join(buildPath, "index.html"),
-          join(buildPath, "404.html")
-        );
-      },
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
